ed4aedcc8bbda816369e69109dced1591153a316
Ербулатов Хамит <90035385+Hamsike2021@users.noreply.github.com>
2021-10-01 12:46:54 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,3 +1,3 @@
-hours, minute, seconds = int(input('Введите кол - во часов: ')), int(input('Введите кол - во минут: ')), int(input('Введите кол - во секунд: '))
-time = hours * 60 * 60 + minute * 60 + seconds
-print(f'Время в пути в секундах: {time} с')
+w = float(input('Введите ваш вес: '))
+h = float(input('Введите ваш рост в метрах: '))
+print(f'Ваш индекс тела: {round(w / (h ** 2))}')
